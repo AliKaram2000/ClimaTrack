e2c8f2aad6e8c6bdaa30de39829be7d6b136038d
ali <ali.kahlifa@gmail>
2026-03-16 14:24:07 +0200

    - Favourites Screen UI constructed

--- app/src/main/java/com/aeinae/climatrack/navigation/NavigationGraph.kt
+++ app/src/main/java/com/aeinae/climatrack/navigation/NavigationGraph.kt
@@ -1,11 +1,12 @@
 package com.aeinae.climatrack.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.aeinae.climatrack.views.AlertsScreen
-import com.aeinae.climatrack.views.FavouriteScreen
+import com.aeinae.climatrack.views.favourites.FavoritesScreen
 import com.aeinae.climatrack.views.home.HomeScreen
 import com.aeinae.climatrack.views.SettingsScreen
 import androidx.compose.ui.Modifier
@@ -18,7 +19,10 @@ fun NavigationHost(modifier: Modifier, navController: NavHostController) {
         modifier = modifier
     ) {
         composable(BottomNavItem.Home.route) { HomeScreen() }
-        composable(BottomNavItem.Favourites.route) { FavouriteScreen() }
+        composable(BottomNavItem.Favourites.route) { FavoritesScreen(
+            onFavoriteClick = { Log.d("NavigationHost", "onFavoriteClick: $it") },
+            onAddClick = {Log.d("NavigationHost", "onAddClick")}
+        ) }
         composable(BottomNavItem.Alerts.route) { AlertsScreen() }
         composable(BottomNavItem.Settings.route) { SettingsScreen() }
     }
